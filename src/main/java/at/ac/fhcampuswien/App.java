3ab7f1b46641d984051347854ea323a6cb218e6a
kenan <kenan.husic@stud.fh-campuswien.ac.at>
2022-03-02 12:32:07 +0100

    jetzt gehts

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,9 +4,9 @@ import org.testng.annotations.Test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("***********");
-        System.out.println("*   Welcome to NewsApp   ***");
-        System.out.println("***********");
+        System.out.println("******************************");
+        System.out.println("*   Welcome to NewsApp   *");
+        System.out.println("******************************");
         System.out.println("Enter what you wanna do:");
         System.out.println("a: Get top headlines austria");
         System.out.println("b: Get all news about bitcoin");
