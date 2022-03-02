11defefea1b3aa5be58ba4d42eed317174506913
kenan <kenan.husic@stud.fh-campuswien.ac.at>
2022-03-02 12:25:39 +0100

    jetzt gehts

---
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -0,0 +1,21 @@
+package at.ac.fhcampuswien;
+
+import org.testng.annotations.Test;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("***********");
+        System.out.println("*   Welcome to NewsApp   ***");
+        System.out.println("***********");
+        System.out.println("Enter what you wanna do:");
+        System.out.println("a: Get top headlines austria");
+        System.out.println("b: Get all news about bitcoin");
+        System.out.println("y: Count articles");
+        System.out.println("q: Quit program");
+    }
+
+    public String message(){
+
+        return "wasbruda";
+    }
+}
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,8 +1,7 @@
 package at.ac.fhcampuswien;
-
-
 import org.testng.annotations.Test;
 
+@Test
 public class AppTest {
     // public void myFirstApp
 }
