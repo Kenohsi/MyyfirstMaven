c0b5ca84cb5108daf738e7e873b069e87935c86f
Taha <tahaimran456@gmail.com>
2022-03-13 12:27:50 +0100

    Article class

---
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -0,0 +1,26 @@
+package at.ac.fhcampuswien;
+
+public class Article {
+    private String author;
+    private String title;
+
+
+public Article(String author, String title){
+     this.author = author;
+     this.title = title;
+
+}
+
+public String getAuthor(){
+    return this.author;
+}
+
+public String getTitle() {
+    return this.title;
+}
+
+@Override
+    public String toString() {
+      return author;
+}
+}
