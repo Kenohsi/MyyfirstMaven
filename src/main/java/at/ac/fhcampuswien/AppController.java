956c7ad785a134d0d5c57ee320f4ea3e91ff32af
ifrim <christina.ifrim@stud.fh-campus.ac.at>
2022-03-13 14:40:01 +0100

    AppController partly solved

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,20 +1,20 @@
 package at.ac.fhcampuswien;
 
+import java.awt.Menu;
 import java.util.List;
 
-public class AppController extends Article {
+public class AppController extends Menu {
     private List<Article> articles;
 
 
-    public AppController(List<Article> articles) {
-        super(articles);
-
+    public AppController(List<Article> articles, AppController controller) {
+        super();
         this.articles = articles;
     }
 
     public void setArticles (List<Article> articles) {
 
-        }
+    }
     public int getArticleCount() {
         return 0;
     }
