10b944bc8ead55bcc954f1b8427c5ca41a2caf97
ifrim <christina.ifrim@stud.fh-campus.ac.at>
2022-03-13 14:35:26 +0100

    Menu solved

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,18 +1,35 @@
 package at.ac.fhcampuswien;
 
-public class Menu {
+public abstract class Menu {
 
     private AppController controller;
-
     private String INVALID_USER_INPUT_MESSAGE;
     private String EXIT_MESSAGE;
 
 
-    public Menu (String INVALID_USER_INPUT_MESSAGE, String EXIT_MESSAGE) {
+    public Menu (String INVALID_USER_INPUT_MESSAGE, String EXIT_MESSAGE, AppController controller) {
+        this.controller = controller;
         this.INVALID_USER_INPUT_MESSAGE = INVALID_USER_INPUT_MESSAGE;
         this.EXIT_MESSAGE = EXIT_MESSAGE;
-    }
-    public void start (){
 
     }
+
+
+    public void start () {
+    }
+    private void handleInput (String Input) {
+    }
+    private void getArticleCount (AppController controller) {
+    }
+    private void getTopHeadlinesAustria (AppController controller) {
+    }
+    private void getAllNewsBitcoin (AppController controller) {
+    }
+    private void printExitMessage () {
+    }
+    private void printInvalidInputMessage () {
+    }
+    private void printMenu () {
+    }
 }
+
