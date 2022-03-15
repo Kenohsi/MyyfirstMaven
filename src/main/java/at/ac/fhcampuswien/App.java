61c0b296410ba6b8c040a449a30ae29addca158e
Taha <tahaimran456@gmail.com>
2022-03-15 02:00:51 +0100

    App Class and Menu Class partly done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,18 +4,8 @@ import org.testng.annotations.Test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("******************************");
-        System.out.println("*   Welcome to NewsApp   *");
-        System.out.println("******************************");
-        System.out.println("Enter what you wanna do:");
-        System.out.println("a: Get top headlines austria");
-        System.out.println("b: Get all news about bitcoin");
-        System.out.println("y: Count articles");
-        System.out.println("q: Quit program");
-    }
+        Menu example = new Menu();
+      example.start();
 
-    public String message(){
-
-        return "wasbruda";
     }
 }
--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,23 +1,40 @@
 package at.ac.fhcampuswien;
 
-public abstract class Menu {
+
+import java.util.Scanner;
+
+public class Menu  {
 
     private AppController controller;
     private String INVALID_USER_INPUT_MESSAGE;
-    private String EXIT_MESSAGE;
+    private  String EXIT_MESSAGE;
+
+      public Menu(){
+          this.INVALID_USER_INPUT_MESSAGE ="Invalid selection!";
+          this.EXIT_MESSAGE = "Bye Bye!";
+      }
 
 
-    public Menu (String INVALID_USER_INPUT_MESSAGE, String EXIT_MESSAGE, AppController controller) {
-        this.controller = controller;
-        this.INVALID_USER_INPUT_MESSAGE = INVALID_USER_INPUT_MESSAGE;
-        this.EXIT_MESSAGE = EXIT_MESSAGE;
 
-    }
+    public  void start() {
+        printMenu();
+        handleInput("");
 
-
-    public void start () {
     }
     private void handleInput (String Input) {
+        Scanner sc = new Scanner(System.in);
+          Input = String.valueOf(sc.next().charAt(0));
+        switch (Input) {
+            case "a" -> getTopHeadlinesAustria(this.controller);
+            case "b" -> getAllNewsBitcoin(this.controller);
+            case "y" -> getArticleCount(this.controller);
+            case "q" -> printExitMessage();
+            default -> {
+                printInvalidInputMessage();
+                start();
+            }
+        }
+
     }
     private void getArticleCount (AppController controller) {
     }
@@ -26,10 +43,20 @@ public abstract class Menu {
     private void getAllNewsBitcoin (AppController controller) {
     }
     private void printExitMessage () {
+        System.out.println(EXIT_MESSAGE);
     }
     private void printInvalidInputMessage () {
+        System.out.println(INVALID_USER_INPUT_MESSAGE);
     }
     private void printMenu () {
+        System.out.println("******************************");
+        System.out.println("*    Welcome to NewsApp   *");
+        System.out.println("******************************");
+        System.out.println("Enter what you wanna do:");
+        System.out.println("a: Get top headlines austria");
+        System.out.println("b: Get all news about bitcoin");
+        System.out.println("y: Count articles");
+        System.out.println("q: Quit program");
     }
 }
 
